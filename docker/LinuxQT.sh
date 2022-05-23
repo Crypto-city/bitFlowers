f73bc091ab15419cc2d8aae0f89e9e80f952d1c1
Hser2bio <38903436+Hser2bio@users.noreply.github.com>
2022-05-23 10:49:29 +0200

    Update LinuxQT.sh

--- docker/LinuxQT.sh
+++ docker/LinuxQT.sh
@@ -3,8 +3,6 @@ sudo apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::=
 build-essential libtool autoconf  libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
 libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
 libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev  libdb5.3++ unzip libzmq5 libssl1.0-dev qt4-qmake libqt4-dev build-essential libboost-dev libboost-system-dev \
-    libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev \
-    libssl-dev
 cd ..
 qmake bitFlowers-qt.pro
 make -j8
